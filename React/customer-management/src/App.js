74ec42585351b997ff660fd468247acecf54e8f0
chimy2 <premet@naver.com>
2020-11-22 02:21:48 +0900

    React Project 3
     - customer management
     - 고객 컴포넌트 구조화

--- React/customer-management/src/App.js
+++ React/customer-management/src/App.js
@@ -1,21 +1,76 @@
 import './App.css';
 import Customer from './components/Customer'
 
-const customer={
+const customer=[{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  // random으로 이미지를 보여주는 사이트로 64*64크기로 설정
   'name': 'chimy',
   'birthday': 951013,
   'gender': '남자',
   'job': '학생'
-}
+},{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  // random으로 이미지를 보여주는 사이트로 64*64크기로 설정
+  'name': '치미',
+  'birthday': 940824,
+  'gender': '여자',
+  'job': '개발자'
+},{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  // random으로 이미지를 보여주는 사이트로 64*64크기로 설정
+  'name': '침침',
+  'birthday': 951124,
+  'gender': '남자',
+  'job': '프리랜서'
+}]
 
 function App() {
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-      />
+    <div>
+      <Customer
+        id={customer[0].id}
+        image={customer[0].image}
+        name={customer[0].name}
+        birthday={customer[0].birthday}
+        gender={customer[0].gender}
+        job={customer[0].job}
+        />
+      <Customer
+        id={customer[1].id}
+        image={customer[1].image}
+        name={customer[1].name}
+        birthday={customer[1].birthday}
+        gender={customer[1].gender}
+        job={customer[1].job}
+        />
+      <Customer
+        id={customer[2].id}
+        image={customer[2].image}
+        name={customer[2].name}
+        birthday={customer[2].birthday}
+        gender={customer[2].gender}
+        job={customer[2].job}
+        />
+        {
+          customer.map(c=>{
+            return(
+              <Customer
+                // map 사용 시 key값이 필요하므로 에러가 발생한다
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+                />
+            );
+          })
+        }
+    </div>
   );
 }
 
--- React/customer-management/src/components/Customer.js
+++ React/customer-management/src/components/Customer.js
@@ -2,19 +2,38 @@ import React from 'react';
 
 function Customer(props){
     return (
-        <div className="customer">
-            <div className="static">
-                <h2>chimy</h2>
-                <p>951013</p>
-                <p>남자</p>
-                <p>학생</p>
-            </div>
-            <div className="variable">
-                <h2>{props.name}</h2>
-                <p>{props.birthday}</p>
-                <p>{props.gender}</p>
-                <p>{props.job}</p>
-            </div>
+        // JSX 문법
+        // 2개 이상의 태그를 return할 때 div 태그로 묶어 반환한다
+        <div>
+            <CustomerProfile 
+                id={props.id}
+                image={props.image}
+                name={props.name}
+                />
+            <CustomerInfo
+                birthday={props.birthday}
+                gender={props.gender}
+                job={props.job}
+                />
+        </div>
+    )
+}
+
+function CustomerProfile(props){
+    return (
+        <div>
+            <img src={props.image} alt="profile" />
+            <h2>{props.name}({props.id})</h2>
+        </div>
+    )
+}
+
+function CustomerInfo(props){
+    return (
+        <div>
+            <p>{props.birthday}</p>
+            <p>{props.gender}</p>
+            <p>{props.job}</p>
         </div>
     )
 }
