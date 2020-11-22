4a17dd7bb216e96c77150a92a1f8e640fc89ea4f
chimy2 <premet@naver.com>
2020-11-22 12:10:36 +0900

    React Project 4
     - customer management
     - Material UI를 이용하여 고객 테이블 디자인

--- React/customer-management/src/App.js
+++ React/customer-management/src/App.js
@@ -1,5 +1,22 @@
 import './App.css';
 import Customer from './components/Customer'
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles=theme=>({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit*3,
+    overflowX: 'auto'
+  },table: {
+    minWidth: 1080
+  }
+})
 
 const customer=[{
   'id': 1,
@@ -27,33 +44,22 @@ const customer=[{
   'job': '프리랜서'
 }]
 
-function App() {
+function App(props) {
+  const {classes}=props;
   return (
-    <div>
-      <Customer
-        id={customer[0].id}
-        image={customer[0].image}
-        name={customer[0].name}
-        birthday={customer[0].birthday}
-        gender={customer[0].gender}
-        job={customer[0].job}
-        />
-      <Customer
-        id={customer[1].id}
-        image={customer[1].image}
-        name={customer[1].name}
-        birthday={customer[1].birthday}
-        gender={customer[1].gender}
-        job={customer[1].job}
-        />
-      <Customer
-        id={customer[2].id}
-        image={customer[2].image}
-        name={customer[2].name}
-        birthday={customer[2].birthday}
-        gender={customer[2].gender}
-        job={customer[2].job}
-        />
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
         {
           customer.map(c=>{
             return(
@@ -70,8 +76,10 @@ function App() {
             );
           })
         }
-    </div>
+        </TableBody>
+      </Table>
+    </Paper>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
