45020dfd10e1505361bfe4a07e3b558f1defb0ed
chimy2 <premet@naver.com>
2020-11-29 08:28:33 +0900

    React Project 14
     - customer management 완성
     - UI/UX 변경
     - Customer Search Module 추가

--- React/customer-management/server.js
+++ React/customer-management/server.js
@@ -4,12 +4,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
 const data = fs.readFileSync('database.json');
 const conf = JSON.parse(data);
 const mysql = require('mysql');
+
+const multer = require('multer');
+const upload = multer({dest: './upload'});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const connection = mysql.createConnection({
   host: conf.host,
@@ -19,9 +23,6 @@ const connection = mysql.createConnection({
   database: conf.database
 });
 connection.connect();
-
-const multer = require('multer');
-const upload = multer({dest: './upload'});
 
 app.get('/api/customers', (req, res)=>{
   connection.query(
